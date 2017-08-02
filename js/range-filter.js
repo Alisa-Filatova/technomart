492970d226b573c9539838ad88679abba19fbd85
Alisa Filatova <alicefill@bk.ru>
2017-08-02 17:45:26 +0300

    добавлен скрипт для ползунка фильтра

---
+++ js/range-filter.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Alisa on 02.08.17.
+ */
+
+var $rangeFilter = $( "#slider-range" );
+
+$( function() {
+    $rangeFilter.slider({
+        range: true,
+        min: 0,
+        max: 30000,
+        values: [ 2500, 20000 ],
+        slide: function( event, ui ) {
+            $( "#amount" ).val( ui.values[ 0 ] + 'р' + " - " + ui.values[ 1 ] + 'р' );
+        }
+    });
+    $( "#amount" ).val( $rangeFilter.slider( "values", 0 ) + 'р' +
+        " - " + $rangeFilter.slider( "values", 1 ) + 'р' );
+} );
+
