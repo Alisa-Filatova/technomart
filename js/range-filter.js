c755fa2176549ca5452f9e772e029db521bed74b
Alisa Filatova <alicefill@bk.ru>
2017-08-02 22:07:58 +0300

    добавлен скрипт для ползунка фильтра

--- js/range-filter.js
+++ js/range-filter.js
@@ -2,19 +2,22 @@
  * Created by Alisa on 02.08.17.
  */
 
-var $rangeFilter = $( "#slider-range" );
+var $rangeFilter = $('#slider-range');
 
-$( function() {
+$(function() {
     $rangeFilter.slider({
         range: true,
         min: 0,
         max: 30000,
-        values: [ 2500, 20000 ],
-        slide: function( event, ui ) {
-            $( "#amount" ).val( ui.values[ 0 ] + 'р' + " - " + ui.values[ 1 ] + 'р' );
+        values: [2500, 20000],
+        slide: function(event, ui) {
+            $('#amount').val(ui.values[ 0 ] + 'р' + ' - ' + ui.values[1] + 'р');
         }
     });
-    $( "#amount" ).val( $rangeFilter.slider( "values", 0 ) + 'р' +
-        " - " + $rangeFilter.slider( "values", 1 ) + 'р' );
-} );
 
+    var value = $rangeFilter.slider('values', 0) + 'р'
+                + ' - ' +
+                $rangeFilter.slider('values', 1) + 'р';
+
+    $('#amount').val(value);
+});
